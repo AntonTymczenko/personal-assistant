a1bdc05f80dfde19dce52800a93f3a983628c147
Anton Tymczenko <a.tymczenko@gmail.com>
2022-07-21 19:37:57 +0300

    feature: add miniaylo banks and points

--- currency-pages/index.js
+++ currency-pages/index.js
@@ -1,4 +1,5 @@
 export * as minfin from './minfin.js';
 export * as miniayloP2p from './miniaylo-p2p.js';
 export * as miniayloBanks from './miniaylo-banks.js';
+export * as miniayloPoints from './miniaylo-points.js';
 
---
+++ currency-pages/miniaylo-points.js
@@ -0,0 +1,30 @@
+export const url = 'https://tables.finance.ua/ua/currency/cash/-/ua/usd/2'; // start page, contains USD banks
+
+const urlEur = 'https://tables.finance.ua/ua/currency/cash/-/ua/eur/2'; // EUR banks
+
+export const crawl = async (page) => {
+  // USD
+  const usdText = await page.evaluate(() => {
+    return document.querySelector('div[class="data-table-footer"] table tr:nth-child(2)').innerText;
+  });
+
+  const usdBid = Number.parseFloat(usdText.split('\t')[0].replace(' ', ''));
+  const usdAsk = Number.parseFloat(usdText.split('\t')[1].replace(' ', ''));
+
+  // EUR
+  await page.goto(urlEur);
+  await page.waitForSelector('div[class="data-table-footer"] table tr:nth-child(2)');
+  await page.screenshot({path: `miniayloPoints-${Date.now()}.png`});
+  const eurText = await page.evaluate(() => {
+    return document.querySelector('div[class="data-table-footer"] table tr:nth-child(2)').innerText;
+  });
+
+  const eurBid = Number.parseFloat(eurText.split('\t')[0].replace(' ', ''));
+  const eurAsk = Number.parseFloat(eurText.split('\t')[1].replace(' ', ''));
+
+  return {
+    usd: { bid: usdBid, ask: usdAsk },
+    eur: { bid: eurBid, ask: eurAsk },
+  };
+};
+
